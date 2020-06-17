10a88be71d110bd3b4aebf4332e4229e3339a21a
Codester291 <Codester291@gmail.com>
2020-06-17 19:07:44 +0100

    added typing animation and replaced favicon

--- script/script.js
+++ script/script.js
@@ -4,9 +4,30 @@ particlesJS.load('particles-js', './assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
 
-document.getElementsByTagName("body").onscroll = function () {myFunction()};
+var name = "Tolani_";
+var wo = "";
+var i = 0;
+var brand = document.getElementById("nav-brand");
+var bann = document.getElementById("word");
+function typewriter () {
+  if (i < name.length) {
+    brand.innerHTML += name.charAt(i);
+    i++;
+    setTimeout(typewriter, 120);
+  }
+}
 
-function myFunction() {
-  var nav = document.getElementsByClassName("nav-bar");
-  nav.style.boxShadow = "0px 13px 34px rgba(0, 0, 0, 0.1)";
+function brandwriter () {
+  if (i < wo.length) {
+    bann.innerHTML += wo.charAt(i);
+    i++;
+    setTimeout(typewriter, 120);
+  }
+}
+
+
+var navbar = document.getElementById("nav-bar");
+
+function shadow() {
+  navbar.style.boxShadow = "0px 13px 34px #f1f1f1";
 }
