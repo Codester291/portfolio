f8475f5eb33b40e66f37c7f9cdf25202fd7ac5a2
Codester291 <Codester291@gmail.com>
2020-06-17 11:43:52 +0100

    modified nav brand fonts and resized footer svgs

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
-// const particlesJS = require('particle.js');
+
+
 particlesJS.load('particles-js', './assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
